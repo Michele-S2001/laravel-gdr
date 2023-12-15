2f8f3469a763eb85b838edd28810b8c92e2dab11
MicheleSerafini <serafini.michele01@gmail.com>
2023-12-15 18:37:15 +0100

    ADD > Basic style changes

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -1,44 +1,39 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.default')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title-name', 'Characters - infos')
 
-    <title>Laravel</title>
+@section('main-content')
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <main class="main-content">
+        <section class="characters-info py-3">
+            <div class="container">
+                <h1>Characters Info</h1>
+                <table class="table table-striped mt-3">
+                    <thead class="table-warning">
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Defense</th>
+                            <th scope="col">Speed</th>
+                            <th scope="col">Hp</th>
+                            <th scope="col">Bio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($characters as $character)
+                            <tr>
+                                <td>{{ $character->name }}</td>
+                                <td>{{ $character->def }}</td>
+                                <td>{{ $character->speed }}</td>
+                                <td>{{ $character->hp }}</td>
+                                <td>{{ $character->bio }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </main>
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
+@endsection
 
-</head>
 
-<body>
-<div class="container mt-5">
-    <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Bio</th>
-            <th scope="col">Defense</th>
-            <th scope="col">Speed</th>
-            <th scope="col">Hp</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach ($characters as $character)
-            <tr>
-                <td> {{ $character->name}}</td>
-                <td> {{ $character->bio}}</td>
-                <td>{{ $character->def}}</td>
-                <td>{{ $character->speed}}</td>
-                <td>{{ $character->hp}}</td>
-            </tr>
-          @endforeach
-        </tbody>
-    </table>
-
-</div>
-</body>
